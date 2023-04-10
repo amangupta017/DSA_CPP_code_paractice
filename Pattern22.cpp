511ee36675ff0e5dccb3f3322e281c80f863e6e7
Aman Gupta <aman.2020@vitbhopal.ac.in>
2023-04-10 23:11:03 +0530

    worked on pattern 22

--- Pattern22.cpp
+++ Pattern22.cpp
@@ -5,16 +5,20 @@ int main(){
     int n;
     cin>>n;
     int i=1;
-    int count=1;
     while (i<=n){
         int j=1;
-        while(j<=i){
-            cout<<count<<" ";
-            count=count+1;
+        int space = 1;
+        while (space<i){
+            cout<<" ";
+            space=space+1;
+
+        }
+        
+        while(j<=n-i+1){
+            cout<<i;
             j=j+1;
         }
         cout<<endl;
         i=i+1;
     }
-    
 }
