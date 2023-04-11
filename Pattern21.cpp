8e683224695f37def51881ed91d2458c6849f5eb
Aman Gupta <aman.2020@vitbhopal.ac.in>
2023-04-11 22:07:01 +0530

    patterns

--- Pattern21.cpp
+++ Pattern21.cpp
@@ -6,18 +6,17 @@ int main(){
     cin>>n;
     int i=1;
     while (i<=n){
-        int space = n-i+1;
-        while (space){
-            cout<<" ";
-            space=space - 1;
-
-        }
-        int j=1;
-        while(j<=i){
-            cout<<"* ";
+       int space=1;
+       while(space<i){
+        cout<<" ";
+        space=space+1;
+       }
+       int j=1;
+       while(j<=n-i+1){
+            cout<<"*";
             j=j+1;
-        }
-        cout<<endl;
-        i=i+1;
+       }
+       cout<<endl;
+       i=i+1;
     }
 }
