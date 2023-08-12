80cdc7dbc1fb6c2e4973ab66f6217d8430f065fb
Aman Gupta <aman.2020@vitbhopal.ac.in>
2023-08-12 17:50:03 +0530

    practice

--- STL_inQueue.cpp
+++ STL_inQueue.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+
+    queue<string> q;
+
+    q.push("aman");
+    q.push("gupta");
+    q.push("hello");
+
+    cout<<"size before pop "<<q.size()<<endl;
+    cout<<"element at front or first : "<<q.front()<<endl;
+    q.pop();
+    cout<<"element at front or first : "<<q.front()<<endl;
+    cout<<"size after pop "<<q.size()<<endl;
+}
