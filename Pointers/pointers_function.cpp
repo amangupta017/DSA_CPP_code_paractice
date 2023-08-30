f206d2d23b5482d1d6fc6f505d70b8f71d12b97c
Aman Gupta <aman.2020@vitbhopal.ac.in>
2023-08-30 10:12:36 +0530

    practice

--- Pointers/pointers_function.cpp
+++ Pointers/pointers_function.cpp
@@ -7,17 +7,32 @@ void print(int *p){
 }
 
 void update(int *p){
-    p=p+1;
-    cout<<"Inside : "<<p<<endl;
+    // p=p+1;
+    // cout<<"Inside : "<<p<<endl;
+    *p=*p+1;
+}
+
+int getSum(int *arr, int n){
+
+
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+
+    }
+    return sum;
 }
 
 int main(){
 
-    int value=5;
-    int *p=&value;
-    print(p);
-    cout<<"Before : "<<p<<endl;
-    update(p);
-    cout<<"After : "<<p<<endl;
+    // int value=5;
+    // int *p=&value;
+    // print(p);
+    // cout<<"Before : "<<*p<<endl;
+    // update(p);
+    // cout<<"After : "<<*p<<endl;
+
+    int arr[6]={1,2,3,4,5,6};
+    cout<<"sum is : "<<getSum(arr+3,3)<<endl;
 
 }
